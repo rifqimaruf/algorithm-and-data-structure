6c13ee545e46a9c066963c85cfb6245fd982814b
rifqimaruf <mrifqim.pd@gmail.com>
2025-01-01 20:48:58 +0700

    feat: update script

--- Day17_LeetCode193/script.sh
+++ Day17_LeetCode193/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Searching for phone numbers..."
-grep -P '^(?:\(\d{3}\)|\d{3}|\{\d{3}\})[ -]?\d{3}-\d{4}$' phoneNumber.txt
+grep -P '^(\d{3}-\d{3}-\d{4}|\(\d{3}\) \d{3}-\d{4})$' phoneNumber.txt
