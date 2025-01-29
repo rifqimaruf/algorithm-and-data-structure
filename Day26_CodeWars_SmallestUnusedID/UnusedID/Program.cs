3d21e6987df1cd21248ff5af27ddfd2780ef1116
rifqimaruf <mrifqim.pd@gmail.com>
2025-01-29 23:42:07 +0700

    feat: add solution Smallest Unused ID

---
+++ Day26_CodeWars_SmallestUnusedID/UnusedID/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Kata
+{
+  public static int NextId(int[] ids)
+  {
+    Array.Sort(ids);
+    Console.WriteLine(string.Join(", ", ids));
+    for (int i = 0; i < ids.Length; i++)
+    {
+      if (ids[i] != i)
+      {
+        return i;
+      }
+    }
+    return ids.Length;
+  }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Kata.NextId(new int[] {0, 1, 2, 3, 5}));
+        Console.WriteLine(Kata.NextId(new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+    }
+}
