4df11f4e4972d98a95e0a8ac4723d339a6dbe9b0
rifqimaruf <mrifqim.id@gmail.com>
2025-04-08 20:53:00 +0700

    feat(file): pyramid from the left

---
+++ Day53_shape3/playground3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if(n == 1) {
+        printf("*\n");
+    }
+    else {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                printf("*");
+                if(j < i-1) {
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }
+        for(int i = n-1; i >= 1; i--) {
+            for(int j = 0; j < i; j++) {
+                printf("*");
+                if(j < i-1) {
+                    printf(" ");
+                }
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
