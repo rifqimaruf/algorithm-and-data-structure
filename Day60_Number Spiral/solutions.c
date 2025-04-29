71eefe557f8181933c72c0fd1d399d1c48398246
rifqimaruf <mrifqim.pd@gmail.com>
2025-04-29 23:49:20 +0700

    feat(file): update solution

--- Day60_Number Spiral/solutions.c
+++ Day60_Number Spiral/solutions.c
@@ -3,9 +3,37 @@
 
 int main()
 {
-    int n, a, b;
+    ll n;
     scanf("%d", &n);
     while(n--)
     {
+        ll y, x;
+        scanf("%d %d", &y, &x);
+        ll z = max(y, x);
+        ll z2 = (z-1) * (z-1), ans;
+        if(z%2)
+        {
+            if(y==z)
+            {
+                ans = z2 + x;
+            } 
+            else
+            {
+                ans = z2+2*z-y;
+            }
+        }
+        else
+        {
+            if(x==z)
+            {
+                ans = z2*y;
+            }
+            else
+            {
+                ans=z2+2*z-x;
+            }
+        }
+        printf("%d \n", ans);
     }
+    return 0;
 }
