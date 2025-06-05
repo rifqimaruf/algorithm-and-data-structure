e1a32b44527ba180be39c75f0408e5b2f90712a9
rifqimaruf <mrifqim.pd@gmail.com>
2025-06-05 12:00:29 +0700

    feat(file): solution MakanBang - BFS

---
+++ Day65_MakanBang/solution.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int N, M, S, T;
+    cin >> N >> M >> S >> T;
+
+    vector<vector<int>> adj(N);
+
+    for (int i = 0; i < M; ++i) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u); 
+    }
+
+   
+    vector<bool> visited(N, false);
+    vector<int> distance(N, -1); 
+
+    queue<int> q;
+    q.push(S);
+    visited[S] = true;
+    distance[S] = 0;
+
+    while (!q.empty()) {
+        int current = q.front();
+        q.pop();
+
+        for (int neighbor : adj[current]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                distance[neighbor] = distance[current] + 1;
+                q.push(neighbor);
+            }
+        }
+    }
+
+    cout << distance[T] << endl;
+
+    return 0;
+}
